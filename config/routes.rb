6054df083a6a8d74f983838dfd4bd973fd31af7e
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2023-12-13 23:35:59 +0000

    <add>usersのルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :post_images, only: [:new, :index, :show, :create, :destroy]
+  resources :users, only: [:edit, :show]
 
+  resources :post_images, only: [:new, :index, :show, :create, :destroy]
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
-
 
 
 end
